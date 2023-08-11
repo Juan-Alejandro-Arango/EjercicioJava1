306d6293dada7392579d71a51de77255183c9bd2
Juan-Alejandro-Arango <133030744+Juan-Alejandro-Arango@users.noreply.github.com>
2023-08-11 09:57:19 -0500

    Add files via upload

---
+++ exerciceejem/exerciceejem/point15/Main.java
@@ -0,0 +1,9 @@
+package point15;
+
+public class Main {
+    public static void main(String[] args) {
+        PatternGenerator patternGenerator = new PatternGenerator();
+        patternGenerator.generatePattern();
+    }
+}
+
---
+++ exerciceejem/exerciceejem/point16/MainNumbers.java
@@ -0,0 +1,12 @@
+package point16;
+
+import src.exercice17.Numbers;
+
+public class MainNumbers {
+    public static void main(String[] args) {
+        Numbers numbers = new Numbers(2,10);
+
+        numbers.showCycle();
+    }
+}
+
